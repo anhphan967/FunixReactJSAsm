74f8a4b1da8adc82a168b72ce31f324424dc0022
Phan Anh Tuan <tuancdt067@gmail.com>
2021-12-31 10:25:07 +0700

    lab3.1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/Menu'
+import React from 'react';
+function App() {
+ 
+  return (
+    <div className="App">
+     <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+    </div>
+  );
+}
+
+export default App;
