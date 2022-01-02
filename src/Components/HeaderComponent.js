aefc8f2faa05af0ccd726581653511e9e1d41220
Phan Anh Tuan <tuancdt067@gmail.com>
2022-01-02 21:16:07 +0700

    update

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import {    Navbar, NavbarBrand, NavItem,
-     NavLink, NavbarToggler,    Collapse, Nav} from 'reactstrap';
-import { Link } from 'react-router-dom'
+      NavbarToggler,    Collapse, Nav} from 'reactstrap';
+import { NavLink } from 'react-router-dom'
 import { useState } from 'react';
 import '../App.css';
 
@@ -16,16 +16,16 @@ function Header() {
                         <Collapse isOpen={toggle} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink tag={Link} to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                    <NavLink  className='nav-link' to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink tag={Link} to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                                    <NavLink className='nav-link'  to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink tag={Link} to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                    <NavLink  className='nav-link'  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink tag={Link} to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                                    <NavLink className='nav-link'  to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
