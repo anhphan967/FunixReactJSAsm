55a16658dcd5cbfd6ad2d77eb63bc0bdd0aa7470
Phan Anh Tuan <tuancdt067@gmail.com>
2022-01-09 21:30:56 +0700

    update

--- src/Components/FormContact.js
+++ src/Components/FormContact.js
@@ -20,97 +20,98 @@ function FormContact() {
   const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val) ? undefined : 'Invalid Email Address';
   const composeValidators = (...validators) => (value) =>
     validators.reduce((error, validator) => error || validator(value), undefined);
-  const Render = ({ handleSubmit, form, values, meta }) => {
-    console.log(meta)
-    return (
-      <form onSubmit={handleSubmit}>
-        <Field name="firstName" validate={composeValidators(required, minLength(3), maxLength(15))}>
-          {({ input, meta }) => (
-            <Row className="form-group">
-              <Label htmlFor="firstname" md={2}>First Name</Label>
-              <Col md={10}>
-                <Input {...input} type="text" placeholder="Username" />
-                {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
-              </Col>
-            </Row>
-          )}
-        </Field>
-        <Field name="LastName" validate={composeValidators(required, minLength(3), maxLength(15))}>
-          {({ input, meta }) => (
-            <Row className="form-group">
-              <Label htmlFor="lastName" md={2}>Last Name</Label>
-              <Col md={10}>
-                <Input {...input} type="text" placeholder="Last Name" />
-                {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
-              </Col>
-            </Row>
-          )}
-        </Field>
-        <Field name="Tel" validate={composeValidators(required, minLength(3), maxLength(15), isNumber)}>
-          {({ input, meta }) => (
-            <Row className="form-group">
-              <Label htmlFor="Tel" md={2}>Contact Tel.</Label>
-              <Col md={10}>
-                <Input {...input} type="text" placeholder="Tel Number" />
-                {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
-              </Col>
-            </Row>
-          )}
-        </Field>
-        <Field name="Email" validate={composeValidators(required, validEmail)}>
-          {({ input, meta }) => (
-            <Row className="form-group">
-              <Label htmlFor="Tel" md={2}>Email</Label>
-              <Col md={10}>
-                <Input {...input} type="text" placeholder="Email" />
-                {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
-              </Col>
-            </Row>
-          )}
-        </Field>
-        <Row className="form-group">
-          <Col md={{ size: 6, offset: 2 }}>
-            <div >
-              <Field name="Agree" component="input" type="checkbox" />
-              <strong>May we contact you?</strong>
-            </div>
-          </Col>
-        </Row>
-
-        <Row className="form-group">
-          <Col md={{ size: 6, offset: 2 }}
-          ><Field name="ContactType" component="select">
-              <option value='Tel'>Tel.</option>
-              <option value=''>Email</option>
-            </Field>
-          </Col>
-        </Row>
-        <Field name="Messenger">
-          {({ input }) => (
-            <Row className="form-group">
-              <Label htmlFor="Messenger" md={2}>Your Feedback</Label>
-              <Col md={10}>
-                <Input {...input} type="textarea" placeholder="Messenger" />
-              </Col>
-            </Row>
-          )}
-        </Field>
-        <Row className="form-group">
-          <Col md={{ size: 10, offset: 2 }}>
-            <Button type="submit" color="primary">
-              Send Feedback
-            </Button>
-          </Col>
-        </Row>
-      </form>
-    )
-  }
+  // const Render = ({ handleSubmit,  meta }) => {
+  //   console.log(meta)
+  //   return (
+  //   
+  //   )
+  // }
   return (
     <Form onSubmit={onSubmit}
       initialValues={formData}
-      component={Render} />
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form onSubmit={handleSubmit}>
+               <Field name="firstName" validate={composeValidators(required, minLength(3), maxLength(15))}>
+                 {({ input, meta }) => (
+                  <Row className="form-group">
+                    <Label htmlFor="firstname" md={2}>First Name</Label>
+                    <Col md={10}>
+                      <Input {...input} type="text" placeholder="Username" />
+                      {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+              <Field name="LastName" validate={composeValidators(required, minLength(3), maxLength(15))}>
+                {({ input, meta }) => (
+                  <Row className="form-group">
+                    <Label htmlFor="lastName" md={2}>Last Name</Label>
+                    <Col md={10}>
+                      <Input {...input} type="text" placeholder="Last Name" />
+                      {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+              <Field name="Tel" validate={composeValidators(required, minLength(3), maxLength(15), isNumber)}>
+                {({ input, meta }) => (
+                  <Row className="form-group">
+                    <Label htmlFor="Tel" md={2}>Contact Tel.</Label>
+                    <Col md={10}>
+                      <Input {...input} type="text" placeholder="Tel Number" />
+                      {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+              <Field name="Email" validate={composeValidators(required, validEmail)}>
+                {({ input, meta }) => (
+                  <Row className="form-group">
+                    <Label htmlFor="Tel" md={2}>Email</Label>
+                    <Col md={10}>
+                      <Input {...input} type="text" placeholder="Email" />
+                      {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+              <Row className="form-group">
+                <Col md={{ size: 6, offset: 2 }}>
+                  <div >
+                    <Field name="Agree" component="input" type="checkbox" />
+                    <strong>May we contact you?</strong>
+                  </div>
+                </Col>
+              </Row>
+      
+              <Row className="form-group">
+                <Col md={{ size: 6, offset: 2 }}
+                ><Field name="ContactType" component="select">
+                    <option value='Tel'>Tel.</option>
+                    <option value=''>Email</option>
+                  </Field>
+                </Col>
+              </Row>
+              <Field name="Messenger">
+                {({ input }) => (
+                  <Row className="form-group">
+                    <Label htmlFor="Messenger" md={2}>Your Feedback</Label>
+                    <Col md={10}>
+                      <Input {...input} type="textarea" placeholder="Messenger" />
+                    </Col>
+                  </Row>
+                )}
+              </Field>
+              <Row className="form-group">
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="primary">
+                    Send Feedback
+                  </Button>
+                </Col>
+              </Row>
+            </form>
+        )} />
   )
 }
 
 export default FormContact
-
